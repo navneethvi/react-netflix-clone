19711e57afb389b64c943c629b7828a90ba08d0d
Navaneeth V <navaneethvinod18@gmail.com>
2024-04-27 18:11:06 +0530

    Navbar component name changed to NavBar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
-import Navbar from './components/Navbar'
+import NavBar from './components/NavBar'
 
 function App() {
   return (
     <div>
-      <Navbar/>
+      <NavBar/>
     </div>
   )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-const Navbar = () => {
+const NavBar = () => {
   return (
     <div className="navbar">
       <img className="logo" src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/1920px-Netflix_2015_logo.svg.png" alt="Netflix Logo" />
@@ -9,4 +9,4 @@ const Navbar = () => {
   );
 };
 
-export default Navbar;
+export default NavBar;
