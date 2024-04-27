7f4b38e226804f984e3c32f3b2c797b28c09aa96
Navaneeth V <navaneethvinod18@gmail.com>
2024-04-27 20:39:43 +0530

    constants file created; banner design; css modified; RowPost design

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import NavBar from './components/NavBar'
+import Banner from './components/Banner'
+import RowPost from './components/RowPost'
 
 function App() {
   return (
     <div>
       <NavBar/>
+      <Banner/>
+      <RowPost/>
     </div>
   )
 }
---
+++ src/constants/constants.js
@@ -0,0 +1,7 @@
+const baseUrl = "https://api.themoviedb.org/3"
+const API_KEY = "9f094a21f169195d0d49221bfae8ec00"
+
+export {
+    baseUrl,
+    API_KEY
+}
